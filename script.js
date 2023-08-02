c249290e9693f9c7f4e01e4ba21aad0afeeafc0f
daffa <daffaulwanm@gmail.com>
2023-08-02 17:47:17 +0700

    edit code

---
+++ script.js
@@ -0,0 +1,11 @@
+// for perkalian
+function cekJawaban() {
+    var jawaban1 = parseInt(document.getElementById("perkalian1").value);
+    var jawaban2 = parseInt(document.getElementById("perkalian2").value);
+    
+    if (jawaban1 === 90 && jawaban2 === 5082) {
+      document.getElementById("hasil").innerHTML = "Anda Benar!";
+    } else {
+      document.getElementById("hasil").innerHTML = "Jawaban Anda Salah. Coba lagi!";
+    }
+  }  
